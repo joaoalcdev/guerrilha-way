e4b2f3d9decf774074b469a428331afde78ff206
João Alcântara <joaosantalcantara@gmail.com>
2022-04-25 14:55:04 -0300

    feat: created Header file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Footer from "../components/Footer"
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 import Image from 'next/image'
 
 const Home: NextPage = () => {
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
 
       <div className="flex min-h-screen flex-col items-center justify-center bg-[#151414]">
         <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+          <Header />
           <h1 className="text-6xl font-bold">
             test
           </h1>
