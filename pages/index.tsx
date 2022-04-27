3e8765963f71a0f35eb8f8fe7589865f5b633cb0
João Alcântara <joaosantalcantara@gmail.com>
2022-04-26 22:43:47 -0300

    feat: created section with cards and dates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import HomeSection from '../components/HomeSection'
 
 const Home: NextPage = () => {
   return (
@@ -10,35 +12,95 @@ const Home: NextPage = () => {
         <title>Guerrilha Way</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <Header />
-      <div className="flex flex-col items-center justify-center bg-[#151414]">
-        <main className="flex w-full flex-col items-center justify-center px-20 text-center">
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-          <h1 className="font-bold text-3xl">lorem ipsum</h1>
-        </main>
+      <div className="bg-[rgba(21,20,20,0.87)]">
+        <Header />
+        <HomeSection />
+        <div className=" flex flex-col items-center justify-center bg-[#805e5e]">
+          <main className="flex w-full flex-col items-center justify-center md:px-32 px-10">
+            <section className="flex w-full flex-col items-center justify-center text-center py-14">
+              <div className="flex w-full flex-col items-start text-start justify-start text-white">
+                <div className="flex flex-row items-center space-x-2 pb-2">
+                  <h1 className="flex font-bold items-center text-lg">
+                    <p className="flex border-2 border-white rounded-full p-1 mr-2">40.</p> GW Abril – 2022
+                  </h1>
+                </div>
+                <div className="w-full flex flex-row space-x-5">
+                  <div className="flex flex-row">
+                    <a href="#!"
+                      target="_blank"
+                      rel="noreferrer noopener">
+                      <Image
+                        src="/assets/thumbnail-card.jpg"
+                        width={400}
+                        height={500}
+                        className="px-0 mx-0" />
+                    </a>
+                  </div>
+                  <div className="flex flex-row">
+                    <a href="#!"
+                      target="_blank"
+                      rel="noreferrer noopener">
+                      <Image
+                        src="/assets/thumbnail-card.jpg"
+                        width={400}
+                        height={500}
+                        className="px-0 mx-0" />
+                    </a>
+                  </div>
+                  <div className="flex flex-row">
+                    <a href="#!"
+                      target="_blank"
+                      rel="noreferrer noopener">
+                      <Image
+                        src="/assets/thumbnail-card.jpg"
+                        width={400}
+                        height={500}
+                        className="px-0 mx-0" />
+                    </a>
+                  </div>
+                  <div className="flex flex-row">
+                    <a href="#!"
+                      target="_blank"
+                      rel="noreferrer noopener">
+                      <Image
+                        src="/assets/thumbnail-card.jpg"
+                        width={400}
+                        height={500}
+                        className="px-0 mx-0" />
+                    </a>
+                  </div>
+                </div>
+                <div className="flex flex-row items-center justify-center w-full">
+                  <div className="flex flex-col md:flex-row md:space-x-28 space-y-12 md:space-y-0">
+                    <div className="flex flex-row w-1/2 md:w-2/3">
+                    </div>
+                    <div className="flex flex-col space-y-4 w-1/2 md:w-full font-light">
+                      <p className="flex flex-row font-light text-md text-left md:text-left h-auto">
+                        {/* <Image
+                          src="/assets/thumbnail-card.jpg"
+                          width={400}
+                          height={500}
+                          className="px-0 mx-0" />
+                        <Image
+                          src="/assets/thumbnail-card.jpg"
+                          width={400}
+                          height={500}
+                          className="px-0 mx-0" />
+                        <Image
+                          src="/assets/thumbnail-card.jpg"
+                          width={400}
+                          height={500}
+                          className="px-0 mx-0" /> */}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </main>
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </>
   )
 }
