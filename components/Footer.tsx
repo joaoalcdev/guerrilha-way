f38229efc2cc6d178787e4f4c584de931a207a1b
João Alcântara <joaosantalcantara@gmail.com>
2022-04-26 22:40:55 -0300

    feat: Add devices sizes footer & added links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,8 +5,8 @@ import InstagramIcon from './icons/Instagram'
 
 export default function Footer() {
   return (
-    <footer className="flex w-full pt-4 text-center lg:text-left bg-[#131212] text-white text-sm">
-      <div className="mx-32 py-10 text-center md:text-left">
+    <footer className="flex w-full pt-4 text-left lg:text-left bg-[#131212] text-white text-sm">
+      <div className="mx-8 md:mx-32 py-10 text-center md:text-left">
         <div className="grid grid-1 md:grid-cols-2 lg:grid-cols-4">
           <div className="">
             <h6 className="
@@ -18,45 +18,49 @@ export default function Footer() {
               justify-center
               md:justify-start
             ">
-              <a href="#!" className="text-white text-sm">
+              <a href="#!" target="_blank" rel="noreferrer noopener" className="text-white text-sm">
                 <Image src="/assets/gw-logo.png" width={158} height={38} />
               </a>
             </h6>
             <div className="flex flex-row space-x-4 pb-8 items-center justify-center md:justify-start md:pl-5">
-              <a href="#!" className="text-white text-sm">
+              <a href="#!" target="_blank" rel="noreferrer noopener" className="text-white text-sm">
                 <InstagramIcon />
               </a>
-              <a href="#!" className="text-white text-sm">
+              <a href="#!" target="_blank" rel="noreferrer noopener" className="text-white text-sm">
                 <FacebookIcon />
               </a>
-              <a href="#!" className="text-white text-sm">
+              <a href="#!" target="_blank" rel="noreferrer noopener" className="text-white text-sm">
                 <YoutubeIcon />
               </a>
             </div>
           </div>
           <div className="">
             <p className="mb-4">
-              <a href="#!" className="text-white text-sm">Termos de uso</a>
+              <a href="#!" target="_blank" rel="noreferrer noopener" className="text-white text-sm">Termos de uso</a>
             </p>
             <p className="mb-4">
-              <a href="#!" className="text-white text-sm">Privacidade</a>
+              <a href="#!" target="_blank" rel="noreferrer noopener" className="text-white text-sm">Privacidade</a>
             </p>
           </div>
           <div className="">
             <p className="mb-4">
-              <a href="#!" className="text-white text-sm">Ajuda</a>
+              <a href="#!" target="_blank" rel="noreferrer noopener" className="text-white text-sm">Ajuda</a>
             </p>
             <p className="mb-4">
-              <a href="#!" className="text-white text-sm">Entre em contato</a>
+              <a href="#!" target="_blank" rel="noreferrer noopener" className="text-white text-sm">Entre em contato</a>
             </p>
           </div>
-          <div className="">
-            <h6 className="font-regular text-sm mb-4 flex justify-center md:justify-start md:text-left">
+          <div className="w-full flex-col">
+            <h6 className="mx-16 md:mx-0 font-light text-sm mb-4 flex justify-center md:justify-start md:text-left">
               Baixe o aplicativo gratuitamente e acesse o conteúdo no seu celular
             </h6>
             <p className="flex items-center justify-center md:justify-start mb-4 gap-2">
-              <Image src="/assets/apple.png" width={180} height={50} />
-              <Image src="/assets/google.png" width={180} height={50} />
+              <a href="https://apps.apple.com/br/app/guerrilha-way/id1542027387" className="text-white text-sm">
+                <Image src="/assets/apple.png" width={180} height={50} />
+              </a>
+              <a href="https://play.google.com/store/apps/details?id=com.wrl.guerrilha_way" className="text-white text-sm">
+                <Image src="/assets/google.png" width={180} height={50} />
+              </a>
             </p>
           </div>
         </div>
